eb85f8db6a5e0ff402ee5943ed596b2be785638f
Simon Hanukaev <semenh@gmail.com>
2019-05-15 15:39:09 +0300

    fixed main.js to work with gatsby Link

--- static/assets/js/main.js
+++ static/assets/js/main.js
@@ -1,185 +1,173 @@
 /*
-	Phantom by Pixelarity
-	pixelarity.com | hello@pixelarity.com
-	License: pixelarity.com/license
-*/
+        Phantom by Pixelarity
+        pixelarity.com | hello@pixelarity.com
+        License: pixelarity.com/license
+    */
 
 (function($) {
 
-	var	$window = $(window),
-		$body = $('body');
+	var $window = $(window);
+	var $body = $('body');
 
 	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+	breakpoints({
+		xlarge: ['1281px', '1680px'],
+		large: ['981px', '1280px'],
+		medium: ['737px', '980px'],
+		small: ['481px', '736px'],
+		xsmall: ['361px', '480px'],
+		xxsmall: [null, '360px']
+	});
 
 	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+	$window.on('load', function() {
+		window.setTimeout(function() {
+			$body.removeClass('is-preload');
+		}, 100);
+	});
 
 	// Touch?
-		if (browser.mobile)
-			$body.addClass('is-touch');
+	if (browser.mobile) {
+		$body.addClass('is-touch');
+	}
 
 	// Forms.
-		var $form = $('form');
+	var $form = $('form');
 
-		// Auto-resizing textareas.
-			$form.find('textarea').each(function() {
+	// Auto-resizing textareas.
+	$form.find('textarea').each(function() {
 
-				var $this = $(this),
-					$wrapper = $('<div class="textarea-wrapper"></div>'),
-					$submits = $this.find('input[type="submit"]');
+		var $this = $(this),
+			$wrapper = $('<div class="textarea-wrapper"></div>'),
+			$submits = $this.find('input[type="submit"]');
 
-				$this
-					.wrap($wrapper)
-					.attr('rows', 1)
-					.css('overflow', 'hidden')
-					.css('resize', 'none')
-					.on('keydown', function(event) {
-
-						if (event.keyCode == 13
-						&&	event.ctrlKey) {
-
-							event.preventDefault();
-							event.stopPropagation();
-
-							$(this).blur();
-
-						}
-
-					})
-					.on('blur focus', function() {
-						$this.val($.trim($this.val()));
-					})
-					.on('input blur focus --init', function() {
-
-						$wrapper
-							.css('height', $this.height());
-
-						$this
-							.css('height', 'auto')
-							.css('height', $this.prop('scrollHeight') + 'px');
-
-					})
-					.on('keyup', function(event) {
-
-						if (event.keyCode == 9)
-							$this
-								.select();
-
-					})
-					.triggerHandler('--init');
-
-				// Fix.
-					if (browser.name == 'ie'
-					||	browser.mobile)
-						$this
-							.css('max-height', '10em')
-							.css('overflow-y', 'auto');
-
-			});
-
-	// Menu.
-		var $menu = $('#menu');
-
-		$menu.wrapInner('<div class="inner"></div>');
-
-		$menu._locked = false;
-
-		$menu._lock = function() {
-
-			if ($menu._locked)
-				return false;
-
-			$menu._locked = true;
-
-			window.setTimeout(function() {
-				$menu._locked = false;
-			}, 350);
-
-			return true;
-
-		};
-
-		$menu._show = function() {
-
-			if ($menu._lock())
-				$body.addClass('is-menu-visible');
-
-		};
-
-		$menu._hide = function() {
-
-			if ($menu._lock())
-				$body.removeClass('is-menu-visible');
-
-		};
-
-		$menu._toggle = function() {
-
-			if ($menu._lock())
-				$body.toggleClass('is-menu-visible');
-
-		};
-
-		$menu
-			.appendTo($body)
-			.on('click', function(event) {
-				event.stopPropagation();
-			})
-			.on('click', 'a', function(event) {
-
-				var href = $(this).attr('href');
-
-				event.preventDefault();
-				event.stopPropagation();
-
-				// Hide.
-					$menu._hide();
-
-				// Redirect.
-					if (href == '#menu')
-						return;
-
-					window.setTimeout(function() {
-						window.location.href = href;
-					}, 350);
-
-			})
-			.append('<a class="close" href="#menu">Close</a>');
-
-		$body
-			.on('click', 'a[href="#menu"]', function(event) {
-
-				event.stopPropagation();
-				event.preventDefault();
-
-				// Toggle.
-					$menu._toggle();
-
-			})
-			.on('click', function(event) {
-
-				// Hide.
-					$menu._hide();
-
-			})
+		$this
+			.wrap($wrapper)
+			.attr('rows', 1)
+			.css('overflow', 'hidden')
+			.css('resize', 'none')
 			.on('keydown', function(event) {
 
-				// Hide on escape.
-					if (event.keyCode == 27)
-						$menu._hide();
+				if (event.keyCode == 13
+					&& event.ctrlKey) {
 
-			});
+					event.preventDefault();
+					event.stopPropagation();
+
+					$(this).blur();
+
+				}
+
+			})
+			.on('blur focus', function() {
+				$this.val($.trim($this.val()));
+			})
+			.on('input blur focus --init', function() {
+
+				$wrapper
+					.css('height', $this.height());
+
+				$this
+					.css('height', 'auto')
+					.css('height', $this.prop('scrollHeight') + 'px');
+
+			})
+			.on('keyup', function(event) {
+
+				if (event.keyCode == 9)
+					$this
+						.select();
+
+			})
+			.triggerHandler('--init');
+
+		// Fix.
+		if (browser.name == 'ie'
+			|| browser.mobile)
+			$this
+				.css('max-height', '10em')
+				.css('overflow-y', 'auto');
+
+	});
+
+	// Menu.
+	var locked = false;
+
+	function lock() {
+
+		if (locked)
+			return false;
+
+		locked = true;
+
+		window.setTimeout(function() {
+			locked = false;
+		}, 350);
+
+		return true;
+
+	}
+
+	function _show() {
+		if (lock()) {
+			$body.addClass('is-menu-visible');
+		}
+	}
+
+	function hide() {
+		if (lock()) {
+			$body.removeClass('is-menu-visible');
+		}
+	}
+
+	function toggle() {
+		if (lock()) {
+			$body.toggleClass('is-menu-visible');
+		}
+	}
+
+
+	$body
+		.on('click', '#menu', function(event) {
+			event.stopPropagation();
+		})
+		.on('click', '#menu a', function(event) {
+
+			var href = $(this).attr('href');
+
+			event.preventDefault();
+			event.stopPropagation();
+
+			// Hide.
+			hide();
+
+			// Redirect.
+			if (href === '#menu') {
+				return;
+			}
+
+			window.setTimeout(function() {
+				window.location.href = href;
+			}, 350);
+
+		});
+
+	$body
+		.on('click', 'a[href="#menu"]', function(event) {
+
+			event.stopPropagation();
+			event.preventDefault();
+
+			toggle();
+		})
+		.on('click', function(event) {
+			hide();
+		})
+		.on('keydown', function(event) {
+			// Hide on escape.
+			if (event.keyCode == 27) {
+				hide();
+			}
+		});
 
 })(jQuery);
